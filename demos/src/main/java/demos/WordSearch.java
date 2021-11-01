34fc59546b951678c9b40271996ec4f8ffc33fc1
ClaytonADavis <davisca@email.wofford.edu>
2021-11-01 15:04:11 -0400

    demo4 11/1 commit

--- demos/src/main/java/demos/WordSearch.java
+++ demos/src/main/java/demos/WordSearch.java
@@ -1,8 +1,102 @@
 package demos;
 
 import edu.wofford.woclo.*;
+import java.util.ArrayList;
 
 public class WordSearch {
+  private int width = 5;
+  private int height = 5;
+  private char[][] grid;
+  private ArrayList<int[]> path;
 
-  public static void main(String... args) {}
+  public WordSearch(String grid) {
+    path = new ArrayList<int[]>();
+    this.grid = new char[height][width];
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        this.grid[i][j] = grid.charAt(width * i + j);
+      }
+    }
+  }
+
+  public WordSearch(String grid, int width, int height) {
+    this.width = width;
+    this.height = height;
+    path = new ArrayList<int[]>();
+    this.grid = new char[height][width];
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        this.grid[i][j] = grid.charAt(width * i + j);
+      }
+    }
+  }
+
+  private boolean locationNotOnPath(int x, int y) {
+    for (int i = 0; i < path.size(); i++) {
+      if (path.get(i)[0] == x && path.get(i)[1] == y) return false;
+    }
+    return true;
+  }
+
+  private boolean isLetterAtLocation(char letter, int x, int y) {
+    if (x < 0 || y < 0 || y >= width || x >= height) {
+      return false;
+    } else if (grid[x][y] == letter && locationNotOnPath(x, y)) {
+      path.add(new int[] {x, y});
+      return true;
+    }
+    return false;
+  }
+
+  private boolean findWordAtLocation(String word, int x, int y) {
+    if (word.length() == 1) {
+      if (isLetterAtLocation(word.charAt(0), y, x)) {
+        return true;
+      }
+      return false;
+    } else {
+      return isLetterAtLocation(word.charAt(0), y, x)
+          && (findWordAtLocation(word.substring(1), x + 1, y)
+              || findWordAtLocation(word.substring(1), x - 1, y)
+              || findWordAtLocation(word.substring(1), x, y + 1)
+              || findWordAtLocation(word.substring(1), x, y - 1));
+    }
+  }
+
+  public String findWord(String word) {
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        path.clear();
+        if (findWordAtLocation(word, i, j)) {
+          return getPathString();
+        }
+      }
+    }
+    return (word + " not found");
+  }
+
+  private String getPathString() {
+    String output = "";
+    int x, y;
+    for (int i = 0; i < path.size(); i++) {
+      x = path.get(i)[0];
+      y = path.get(i)[1];
+      output += grid[x][y] + ":" + Integer.toString(x + 1) + "," + Integer.toString(y + 1);
+      if (i < path.size() - 1) output += " ";
+    }
+    return output;
+  }
+
+  public static void main(String... args) {
+    String gridString = "softsweskaolzilklqmtreyoy";
+    String word = "eskimo";
+    int width = 5;
+    int height = 5;
+
+    // TODO implement parser library to set gridString, word, width, and height based off of values
+    // pulled from command line
+
+    WordSearch test = new WordSearch(gridString, width, height);
+    System.out.println(test.findWord(word));
+  }
 }
---
+++ demos/src/test/java/demos/WordSearchTest.java
@@ -0,0 +1,63 @@
+package demos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class WordSearchTest {
+  @Test
+  void testFindWord() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy", 5, 5);
+    assertEquals("e:2,2 s:2,3 k:2,4 i:3,4 m:4,4 o:5,4", test.findWord("eskimo"));
+  }
+
+  @Test
+  void testFindWordSpace() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy", 5, 5);
+    assertEquals("  not found", test.findWord(" "));
+  }
+
+  @Test
+  void testFindWordCap() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy", 5, 5);
+    assertEquals("Eskimo not found", test.findWord("Eskimo"));
+  }
+
+  @Test
+  void testFindWordNotFound() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy", 5, 5);
+    assertEquals("cool not found", test.findWord("cool"));
+  }
+
+  @Test
+  void testFindWordLoop() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy", 5, 5);
+    assertEquals("soews not found", test.findWord("soews"));
+  }
+
+  @Test
+  void testFindWordLetter() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy", 5, 5);
+    assertEquals("s:1,1", test.findWord("s"));
+  }
+
+  @Test
+  void testFindWord2() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy", 5, 5);
+    assertEquals("saoky not found", test.findWord("saoky"));
+  }
+
+  @Test
+  void testFindWord3() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy", 5, 5);
+    assertEquals(
+        "f:1,3 t:1,4 k:2,4 i:3,4 m:4,4 q:4,3 l:4,2 l:3,2 e:2,2 s:2,3 z:3,3",
+        test.findWord("ftkimqllesz"));
+  }
+
+  @Test
+  void testFindWord4() {
+    WordSearch test = new WordSearch("softsweskaolzilklqmtreyoy");
+    assertEquals("altyoyerkowsoftb not found", test.findWord("altyoyerkowsoftb"));
+  }
+}
